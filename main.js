7772581d3653b09ffbd854c81fc06290d432026d
James-NewtonIII <jacenewt57@yahoo.com>
2019-04-16 09:53:20 -0400

    Uploaded all updated files needed for the game.
    I had to go in and change phyisijs_worker.js. Use this version instead of the one that comes from online with phyisijs. There may be warnings for unrolling the loop in webgl. This is because we use LambertMaterial instead of BasicMaterial. This does not cause any real problems.

---
+++ main.js
@@ -0,0 +1,210 @@
+
+function worker_function() {
+    // all code here
+	var width = window.innerWidth;
+var height = window.innerHeight;
+
+
+//declaring physijs and ammojs
+Physijs.scripts.worker = '/js/physijs_worker.js';
+Physijs.scripts.ammo = '/js/ammo.js';
+
+
+
+
+var renderer = new THREE.WebGLRenderer({ antialias: true });
+renderer.setSize(width, height);
+renderer.setClearColor(0x63ddff);
+document.body.appendChild(renderer.domElement);
+ 
+// create scene object
+var scene = new Physijs.Scene;
+
+
+var groundGeometry = new THREE.PlaneGeometry(1000, 1000);
+var groundMaterial = new THREE.MeshLambertMaterial( {color: 0x00bf0f, side: THREE.DoubleSide});
+var ground = new THREE.Mesh(groundGeometry, groundMaterial);
+ground.lookAt(new THREE.Vector3(0, 1, 0));
+ground.position.y = -10;
+scene.add(ground);
+
+
+
+//creates a single pyramid
+var pyramidGeo = new THREE.CylinderGeometry(0, 40, 60, 4);
+var pyramid = new Physijs.CylinderMesh(pyramidGeo, new THREE.MeshLambertMaterial({ color: 0xffffff}));
+pyramid.position.set(300, 0, -300);
+scene.add(pyramid);
+
+//randomly position 100 boxes in a 300x300 square
+for(var i = 0; i < 50; i++){	
+	var box1 = new Physijs.BoxMesh(
+			new THREE.CubeGeometry(3, 20, 3),	
+			new THREE.MeshLambertMaterial({ color: 0xff0000})
+	);
+	
+	box1.position.set(Math.random() * 1000 - 500, 0, Math.random() * 1000 - 500);
+	scene.add(box1);
+	
+	var box2 = new Physijs.BoxMesh(
+		new THREE.CubeGeometry(3, 10, 3),
+		new THREE.MeshLambertMaterial({ color: 0xaaaaaa})
+	);
+	box2.position.set(Math.random() * 1000 - 500, -5, Math.random() * 1000 - 500);
+	scene.add(box2);
+}
+//place 5 more larger boxes
+for(var i = 0; i < 5; i++){	
+	var tower = new Physijs.BoxMesh(
+				 new THREE.CubeGeometry(5, 30, 5),
+				 new THREE.MeshLambertMaterial({ color: 0xeeeeee})
+	);
+	tower.position.set(Math.random() * 1000 - 500, 5, Math.random() * 1000 - 500);
+	scene.add(tower);
+}
+var tower2 = new Physijs.BoxMesh(
+			new THREE.CubeGeometry(5, 40, 5),
+			new THREE.MeshLambertMaterial({ color: 0xffffff})
+	);
+tower2.position.set(10, 10, -150);
+scene.add(tower2);
+
+
+ 
+// add lighting and add to scene
+var directionalLight = new THREE.DirectionalLight(0xffffff, 1);
+directionalLight.position.set(1000, 50, 300).normalize();
+scene.add(directionalLight);
+var ambientLight = new THREE.AmbientLight(0x202020);
+scene.add(ambientLight);
+
+// create perspective camera
+var camera = new THREE.PerspectiveCamera(45, width / height, 0.1, 10000);
+camera.position.x = 0;
+camera.position.y = 5;
+camera.position.z = 40;
+// add to scene and renderer
+scene.add(camera); 
+renderer.render(scene, camera);
+camera.lookAt(tower2.position);
+
+
+
+
+
+var firstMouseMove  = true;
+
+var camPos = {x: 0, y: 0, z: -1};
+
+var camMove = new THREE.Vector3(0, 0, 0);
+var camStrafe = new THREE.Vector3(0, 0, 0);
+
+var cameraLookAt = new THREE.Vector3(0, 0, -1);
+var cameraRight = new THREE.Vector3(1, 0, 0);
+var cameraUp = new THREE.Vector3().crossVectors(cameraRight, cameraLookAt);
+
+var oldMousePos = {x: 0, y: 0};
+
+function handleMouseMove(event) {
+	if(firstMouseMove) {
+		oldMousePos.x = event.clientX;
+		oldMousePos.y = event.clientY;
+		firstMouseMove = false;
+		return;
+	}
+
+	var yaw = (oldMousePos.x - event.clientX) / 200.0;
+	var pitch = (oldMousePos.y - event.clientY) / 200.0;
+	
+	cameraLookAt.applyAxisAngle(new THREE.Vector3(0, 1, 0), yaw);
+	cameraRight.applyAxisAngle(new THREE.Vector3(0, 1, 0), yaw);
+	
+	cameraLookAt.applyAxisAngle(cameraRight, pitch);
+	
+	
+	oldMousePos.x = event.clientX;
+	oldMousePos.y = event.clientY;
+	
+		
+}
+
+
+function doKeyUp(evt) {
+	var code = evt.keyCode;
+	switch(code) {
+		case 65: // a
+			camStrafe.x = 0;  camStrafe.y = 0; camStrafe.z = 0;
+			break;
+		case 68: // d
+			camStrafe.x = 0;  camStrafe.y = 0; camStrafe.z = 0;
+			break;
+		case 87:
+			camMove.x = 0; camMove.y = 0; camMove.z = 0;
+			break;
+		case 83:
+			camMove.x = 0; camMove.y = 0; camMove.z = 0;
+			break;
+	}
+}
+
+function doKeyDown(evt) {
+		var code = evt.keyCode;
+	switch(code) {
+		case 65: // a
+			camStrafe.x = -cameraRight.x / 1.0;
+			camStrafe.y = -cameraRight.y / 1.0;
+			camStrafe.z = -cameraRight.z / 1.0;
+			break;
+		case 68: // d
+			camStrafe.x = cameraRight.x / 1.0;
+			camStrafe.y = cameraRight.y / 1.0;
+			camStrafe.z = cameraRight.z / 1.0;
+			break;
+		case 87: // w 
+			camMove.x = cameraLookAt.x / 1.0;
+			camMove.y = cameraLookAt.y / 1.0;
+			camMove.z = cameraLookAt.z / 1.0;
+			break;
+		case 83: // s
+			camMove.x = -cameraLookAt.x / 1.0;
+			camMove.y = -cameraLookAt.y / 1.0;
+			camMove.z = -cameraLookAt.z / 1.0;
+			break;
+		
+	}
+
+}
+
+
+document.addEventListener('mousemove', handleMouseMove, false);
+
+document.addEventListener('keydown', doKeyDown, false);
+document.addEventListener('keyup', doKeyUp, false);
+
+
+
+
+ 
+renderer.render(scene, camera);
+function render() {
+
+
+	camera.position.add(camMove);
+	camera.position.add(camStrafe);
+	
+
+	var newLookAt = new THREE.Vector3().addVectors(camera.position, cameraLookAt);
+	camera.lookAt(newLookAt);
+	camera.up = cameraUp;
+	
+    scene.simulate(); // run physics
+	renderer.render( scene, camera); // render the scene
+	requestAnimationFrame( render );
+
+	
+}
+render();
+console.clear();
+}
+
+worker_function();
